c0c772852f06b209cce7a158a1f3c714ea75d90c
Gabriel Mocelin <gabriel.mocelin@acad.pucrs.br>
2018-03-22 15:26:38 -0300

    trying to find the right angle for game anchors

--- SolarSystem/SolarSystem/ARScenes/GameScene.swift
+++ SolarSystem/SolarSystem/ARScenes/GameScene.swift
@@ -35,10 +35,12 @@ extension GameScene: ARSCNViewDelegate{
     func generateAnchors(withCameraPosition camera: (SCNVector3, SCNVector3)) -> [GameAnchor] {
         var anchors: [GameAnchor] = []
         
-        var translation = matrix_identity_float4x4
-        translation.columns.3.x += -0.1 * camera.0.x
-        translation.columns.3.y += (camera.0.y - camera.1.y) / 2
-        translation.columns.3.z += 0.25 * camera.0.z
+        var cameraOriginalTranslation = matrix_identity_float4x4
+        cameraOriginalTranslation.columns.3.x += -0.2 * camera.1.x
+        cameraOriginalTranslation.columns.3.y += (camera.0.y - camera.1.y) / 2
+        cameraOriginalTranslation.columns.3.z += 0.25 * camera.0.z
+        
+        var translation = cameraOriginalTranslation
         
         //spaceship or barriers size
         for side in 0..<2{
@@ -48,10 +50,11 @@ extension GameScene: ARSCNViewDelegate{
                     let anchor = GameAnchor(transform: translation, anchorPosition: position, anchorSide: side)
                     anchors.append(anchor)
                 }
-                translation.columns.3.x += 0.1
+                translation.columns.3.x += 0.2
             }
-            translation.columns.3.x += -0.1 * camera.0.x
-            translation.columns.3.z += 0.6 * camera.0.z
+            translation = cameraOriginalTranslation
+            translation.columns.3.x += -0.2 * camera.1.x
+            translation.columns.3.z += 1.1 * camera.0.z
         }
         
         return anchors
@@ -64,7 +67,7 @@ extension GameScene: ARSCNViewDelegate{
         case .spaceship:
             return SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.1))
         case .barriers:
-            return SCNNode(geometry: SCNCone(topRadius: 0.02, bottomRadius: 0.1, height: 0.1))
+            return SCNNode(geometry: SCNCone(topRadius: 0.02, bottomRadius: 0.05, height: 0.05))
         }
     }
 }
