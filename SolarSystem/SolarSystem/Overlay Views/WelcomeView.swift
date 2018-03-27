4a7a7a85476a9d47b90f5725a5418c2bb8cecf80
Gabriel Mocelin <gabriel.mocelin@acad.pucrs.br>
2018-03-27 11:05:56 -0300

    parcial layout welcomeview

--- SolarSystem/SolarSystem/Overlay Views/WelcomeView.swift
+++ SolarSystem/SolarSystem/Overlay Views/WelcomeView.swift
@@ -11,8 +11,54 @@ import UIKit
 class WelcomeView: UIView {
     var myState: ControlState
     
-    var text: UITextView!
-    var readyButton: UIButton!
+    var welcomeView: UIView = {
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+        return view
+    }()
+    
+    var descriptionText: UITextView = {
+        let textView = UITextView()
+        textView.text = "For the best experience be sure you are in a well-lit area."
+        textView.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        textView.textAlignment = .center
+        textView.backgroundColor = UIColor.clear
+        return textView
+    }()
+    
+    var welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Welcome to my playground"
+        label.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    var readyButton: UIButton = {
+        let button = UIButton(frame: CGRect())
+        button.setTitle("I'm Ready", for: .normal)
+        button.addTarget(self, action: #selector(handleReadyButton), for: .touchUpInside)
+        button.backgroundColor = #colorLiteral(red: 0.9860219359, green: 0.4115800261, blue: 0.3854584694, alpha: 1)
+        button.layer.cornerRadius = 30
+        button.clipsToBounds = true
+        return button
+    }()
+    
+    var trackingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Tracking State"
+        label.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    var trackingState: UILabel = {
+        let label = UILabel()
+        label.text = "GOOD"
+        label.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        label.textAlignment = .center
+        return label
+    }()
     
     var stateDelegate: StateManager?
 
@@ -21,12 +67,60 @@ class WelcomeView: UIView {
         
         super.init(frame: frame)
         setupBackgroundBlur()
-        setupTextField()
         setupReadyButton()
+        setupWelcomeView()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupWelcomeView() {
+        
+        self.addSubview(welcomeView)
+        welcomeView.translatesAutoresizingMaskIntoConstraints = false
+        welcomeView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        welcomeView.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -30).isActive = true
+        welcomeView.heightAnchor.constraint(equalToConstant: 280).isActive = true
+        welcomeView.widthAnchor.constraint(equalToConstant: 350).isActive = true
+        
+        
+        welcomeView.addSubview(welcomeLabel)
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        welcomeLabel.centerXAnchor.constraint(equalTo: welcomeView.centerXAnchor).isActive = true
+        welcomeLabel.widthAnchor.constraint(equalTo: welcomeView.widthAnchor, constant: 0).isActive = true
+        welcomeLabel.topAnchor.constraint(equalTo: welcomeView.topAnchor, constant: 20).isActive = true
+        welcomeLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        welcomeView.addSubview(descriptionText)
+        descriptionText.translatesAutoresizingMaskIntoConstraints = false
+        descriptionText.centerXAnchor.constraint(equalTo: welcomeView.centerXAnchor).isActive = true
+        descriptionText.widthAnchor.constraint(equalTo: welcomeView.widthAnchor, constant: 0).isActive = true
+        descriptionText.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 20).isActive = true
+        descriptionText.heightAnchor.constraint(equalToConstant: 20).isActive = true
+       
+        let separatorLine = UIView()
+        separatorLine.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        welcomeView.addSubview(separatorLine)
+        separatorLine.translatesAutoresizingMaskIntoConstraints = false
+        separatorLine.centerXAnchor.constraint(equalTo: welcomeView.centerXAnchor).isActive = true
+        separatorLine.topAnchor.constraint(equalTo: descriptionText.bottomAnchor, constant: 30).isActive = true
+        separatorLine.heightAnchor.constraint(equalToConstant: 3).isActive = true
+        separatorLine.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        welcomeView.addSubview(trackingLabel)
+        trackingLabel.translatesAutoresizingMaskIntoConstraints = false
+        trackingLabel.centerXAnchor.constraint(equalTo: welcomeView.centerXAnchor).isActive = true
+        trackingLabel.widthAnchor.constraint(equalTo: welcomeView.widthAnchor, constant: 0).isActive = true
+        trackingLabel.topAnchor.constraint(equalTo: separatorLine.bottomAnchor, constant: 20).isActive = true
+        trackingLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        welcomeView.addSubview(trackingState)
+        trackingState.translatesAutoresizingMaskIntoConstraints = false
+        trackingState.centerXAnchor.constraint(equalTo: welcomeView.centerXAnchor).isActive = true
+        trackingState.widthAnchor.constraint(equalTo: welcomeView.widthAnchor, constant: 0).isActive = true
+        trackingState.topAnchor.constraint(equalTo: trackingLabel.bottomAnchor, constant: 20).isActive = true
+        trackingState.heightAnchor.constraint(equalToConstant: 20).isActive = true
     }
     
     func setupBackgroundBlur() {
@@ -38,33 +132,21 @@ class WelcomeView: UIView {
     }
     
     func setupTextField() {
-        text = UITextView(frame: CGRect())
-        text.text = "Welcome to my playground, here you gonna use ARkit, so be sure you are in a good place to test it. Press ready to continue"
-        text.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        text.backgroundColor = UIColor.clear
-        
-        self.addSubview(text)
-        text.translatesAutoresizingMaskIntoConstraints = false
-        text.heightAnchor.constraint(equalToConstant: 300).isActive = true
-        text.widthAnchor.constraint(equalToConstant: 300).isActive = true
-        text.topAnchor.constraint(equalTo: self.topAnchor, constant: 100).isActive = true
-        text.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 50).isActive = true
+        self.addSubview(descriptionText)
+        descriptionText.translatesAutoresizingMaskIntoConstraints = false
+        descriptionText.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        descriptionText.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        descriptionText.topAnchor.constraint(equalTo: self.topAnchor, constant: 100).isActive = true
+        descriptionText.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 50).isActive = true
     }
     
     func setupReadyButton() {
-        readyButton = UIButton(frame: CGRect())
-        readyButton.setTitle("I'm Ready", for: .normal)
-        readyButton.addTarget(self, action: #selector(handleReadyButton), for: .touchUpInside)
-        readyButton.backgroundColor = #colorLiteral(red: 0.9860219359, green: 0.4115800261, blue: 0.3854584694, alpha: 1)
-        readyButton.layer.cornerRadius = 30
-        readyButton.clipsToBounds = true
-        
         self.addSubview(readyButton)
         readyButton.translatesAutoresizingMaskIntoConstraints = false
         readyButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         readyButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
         readyButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
-        readyButton.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 0).isActive = true
+        readyButton.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
     }
     
     @objc func handleReadyButton() {
