8791b256fc6e567ed2a326b74bba077596a22fca
Gabriel Mocelin <gabriel.mocelin@acad.pucrs.br>
2018-03-28 18:39:45 -0300

    starting input the font

---
+++ SolarSystem/SolarSystem/Helpers/Font.swift
@@ -0,0 +1,14 @@
+//
+//  Font.swift
+//  SolarSystem
+//
+//  Created by Gabriel Mocelin on 28/03/18.
+//  Copyright © 2018 Gabriel Mocelin. All rights reserved.
+//
+
+import Foundation
+
+struct font {
+    static let bold = 1
+    static let regular = 2
+}
